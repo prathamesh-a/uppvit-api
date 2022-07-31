97a861301b8b7b298434d47ddd4beb0e07eacecd
prathamesh-a <89336149+prathamesh-a@users.noreply.github.com>
2022-07-31 19:44:05 +0530

    spring boot init

---
+++ src/main/java/inix/UppvitSpringApplication.java
@@ -0,0 +1,13 @@
+package inix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UppvitSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UppvitSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/inix/UppvitSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package inix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UppvitSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
